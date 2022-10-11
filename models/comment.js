8c49390100f47003ddc08f2cfdb12da3c7dd4c6a
kevinkorza <kevingkorza@gmail.com>
2022-10-11 12:16:01 -0400

    comment.js creation

---
+++ models/comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class comment extends Model {}
+
+comment.init({
+    id:
+    {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    text: 
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: 
+        {
+            model: 'user',
+            key: 'id'
+
+        }
+
+    },
+    postId: 
+    {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:
+        {
+            model: 'post',
+            key: 'id'
+        }
+
+    },
+
+   sequelize,
+   timestamps: false,
+   freezeTableName: true,
+   underscored: true,
+   modelName: 'comment',
+});
+
+model.exports = comment;
